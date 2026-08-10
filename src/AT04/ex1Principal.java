e3c257f39a1da0a1f44805885beeb5405416b209
Thiago Varoni <thiago.varoni.araujo@gmail.com>
2026-08-10 20:59:33 -0300

    adiciona os dois arquivos do ex1 da AT04

---
+++ src/AT04/ex1Principal.java
@@ -0,0 +1,6 @@
+public class Principal {
+    public static void main(String[] args) {
+        ex1Produto produto1 = new ex1Produto("Notebook pica", 5500.00);
+        produto1.exibirDetalhes();
+    }
+}
