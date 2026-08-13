d2385c75dafd56cd50622b064882436cd484ce20
Thiago Varoni <thiago.varoni.araujo@gmail.com>
2026-08-13 20:25:57 -0300

    Adiciona calculadora de matrizes com funções: printar matriz, somar duas matrizes A e B , Subtrair duas matrizes A e B

---
+++ src/matrizSuprema/CalculadoraMatrizes.java
@@ -0,0 +1,149 @@
+package matrizSuprema;
+
+import java.util.Scanner;
+public class CalculadoraMatrizes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n=========================================");
+        System.out.println("         CALCULADORA DE MATRIZES         ");
+        System.out.println("=========================================");
+        System.out.println("1 - Ler e Imprimir Matriz");
+        System.out.println("2 - Somar Matrizes (A + B)");
+        System.out.println("3 - Subtrair Matrizes (A - B)");
+        System.out.println("4 - Multiplicar por Escalar (k * A)");
+        System.out.println("5 - Transposta de uma Matriz (A^T)");
+        System.out.println("6 - Multiplicação de Matrizes (A * B)");
+        System.out.println("7 - Gerar Matriz Identidade");
+        System.out.println("8 - Verificar se é Matriz Diagonal");
+        System.out.println("9 - Verificar se é Matriz Simétrica");
+        System.out.println("0 - Sair");
+        System.out.println("=========================================");
+        System.out.print("Digite a opção desejada: ");
+        int opcao = sc.nextInt();
+
+        switch (opcao){
+            case 1: {
+                System.out.print("Digite o quantidade de linhas da matriz: ");
+                int linha = sc.nextInt();
+                System.out.print("Digite o quantidade de colunas da matriz: ");
+                int coluna = sc.nextInt();
+
+                int[][] matriz = new int[linha][coluna];
+
+                for (int i = 0; i < linha; i++) {
+                    for (int j = 0; j < coluna; j++) {
+                        System.out.print("Digite o valor da posição " + i + " " + j + ": ");
+                        matriz[i][j] = sc.nextInt();
+                    }
+                }
+
+                sc.nextLine();
+                System.out.print("\nPressione [ENTER] para ver o resultado");
+                sc.nextLine();
+
+                for (int i = 0; i < linha; i++) {
+                    for (int j = 0; j < coluna; j++) {
+                        System.out.print(matriz[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            }
+
+            case 2: {
+                System.out.print("Digite o quantidade de linhas da matriz A: ");
+                int linhaA = sc.nextInt();
+                System.out.print("Digite o quantidade de colunas da matriz A: ");
+                int colunaA = sc.nextInt();
+                int[][] matrizA = new int[linhaA][colunaA];
+
+                System.out.print("Digite o quantidade de linhas da matriz B: ");
+                int linhaB = sc.nextInt();
+                System.out.print("Digite o quantidade de colunas da matriz B: ");
+                int colunaB = sc.nextInt();
+
+                if (linhaA != linhaB || colunaA != colunaB) {
+                    System.out.println("Erro: Só é possível somar matrizes com as mesmas dimensões!");
+                } else {
+
+                    for (int i = 0; i < linhaA; i++) {
+                        for (int j = 0; j < colunaA; j++) {
+                            System.out.print("Digite o valor da posição " + i + " " + j + ": ");
+                            matrizA[i][j] = sc.nextInt();
+                        }
+                    }
+                    int[][] matrizB = new int[linhaB][colunaB];
+                    for (int i = 0; i < linhaB; i++) {
+                        for (int j = 0; j < colunaB; j++) {
+                            System.out.print("Digite o valor da posição " + i + " " + j + ": ");
+                            matrizB[i][j] = sc.nextInt();
+                        }
+                    }
+
+                    int[][] matrizC = new int[linhaA][colunaA];
+                    for (int i = 0; i < linhaA; i++) {
+                        for (int j = 0; j < colunaA; j++) {
+                            matrizC[i][j] = matrizA[i][j] + matrizB[i][j];
+                        }
+                    }
+
+                    for (int i = 0; i < linhaA; i++) {
+                        for (int j = 0; j < colunaA; j++) {
+                            System.out.print(matrizC[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                }
+                break;
+            }
+
+            case 3: {
+                System.out.print("Digite o quantidade de linhas da matriz A: ");
+                int linhaA = sc.nextInt();
+                System.out.print("Digite o quantidade de colunas da matriz A: ");
+                int colunaA = sc.nextInt();
+                int[][] matrizA = new int[linhaA][colunaA];
+
+                System.out.print("Digite o quantidade de linhas da matriz B: ");
+                int linhaB = sc.nextInt();
+                System.out.print("Digite o quantidade de colunas da matriz B: ");
+                int colunaB = sc.nextInt();
+
+                if (linhaA != linhaB || colunaA != colunaB) {
+                    System.out.println("Erro: Só é possível subtrair matrizes com as mesmas dimensões!");
+                } else {
+
+                    for (int i = 0; i < linhaA; i++) {
+                        for (int j = 0; j < colunaA; j++) {
+                            System.out.print("Digite o valor da posição " + i + " " + j + ": ");
+                            matrizA[i][j] = sc.nextInt();
+                        }
+                    }
+                    int[][] matrizB = new int[linhaB][colunaB];
+                    for (int i = 0; i < linhaB; i++) {
+                        for (int j = 0; j < colunaB; j++) {
+                            System.out.print("Digite o valor da posição " + i + " " + j + ": ");
+                            matrizB[i][j] = sc.nextInt();
+                        }
+                    }
+
+                    int[][] matrizC = new int[linhaA][colunaA];
+                    for (int i = 0; i < linhaA; i++) {
+                        for (int j = 0; j < colunaA; j++) {
+                            matrizC[i][j] = matrizA[i][j] - matrizB[i][j];
+                        }
+                    }
+
+                    for (int i = 0; i < linhaA; i++) {
+                        for (int j = 0; j < colunaA; j++) {
+                            System.out.print(matrizC[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
